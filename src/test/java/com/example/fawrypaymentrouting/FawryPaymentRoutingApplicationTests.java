823686dc98a7165c77f2ab8a326d20dd1c6a8cbe
Tony-Elia <antony.safaga@gmail.com>
2026-05-20 20:42:35 +0300

    chore: project init and base configurations

---
+++ src/test/java/com/example/fawrypaymentrouting/FawryPaymentRoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fawrypaymentrouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FawryPaymentRoutingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
